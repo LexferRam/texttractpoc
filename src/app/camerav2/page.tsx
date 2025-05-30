51983d3ec40b82333d32ea094a52fb89e5bfcc12
Lexfer <lexferramirez@gmail.com>
2025-05-30 12:19:25 -0400

    fix prompt

--- src/app/camerav2/page.tsx
+++ src/app/camerav2/page.tsx
@@ -15,7 +15,7 @@ const App: React.FC = () => {
             <hr />
             <ButtonsActions
                 description="Carnet de circulación o Título de propiedad"
-                promptText={'obten la siguiente informacion de la imagen y retorna en formato JSON(valida realmente sea un carnet de circulación de venezuela, si no es un documento válido retorna el mensaje: "Documento inválido"): año del vehiculo, marca, modelo, placa, serial de motor(está arriba del nombre del titular), serial carroceria N.I.V o NIV, color del carro, destinado(ejemplo: particular etc), grupo(ejemplo: sport wagon, sedan etc), version(normalmente está junto con el modelo)'}
+                promptText={'obten la siguiente informacion de la imagen y retorna en formato JSON(valida realmente sea un carnet de circulación de venezuela, si no es un documento válido retorna el mensaje: "Documento inválido"): año del vehiculo, marca, modelo, placa, serial de motor(está arriba del nombre del titular), serial carroceria N.I.V o NIV, color del carro, destinado o uso (ejemplo: particular, transporte público, etc), grupo(ejemplo: sport wagon, sedan etc), version(normalmente está junto con el modelo)'}
                 capture={false}
             />
         </div>
