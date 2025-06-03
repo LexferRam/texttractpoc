360baa8efae6c33ab43c18ed87e2a35560574085
Lexfer <lexferramirez@gmail.com>
2025-06-03 09:49:24 -0400

    fix

--- src/app/camerav2/page.tsx
+++ src/app/camerav2/page.tsx
@@ -5,33 +5,10 @@ import ButtonsActions from './_components/ButtonsActions';
 import { prompts } from './const/prompts';
 
 const App: React.FC = () => {
-      const [localIp, setLocalIp] = useState('');
-  console.log('🚀 ~ App ~ localIp:', localIp);
 
   useEffect(() => {
-    const getLocalIP = async () => {
-      const ipRegex = /([0-9]{1,3}\.){3}[0-9]{1,3}/;
-
-      const pc = new RTCPeerConnection({
-        iceServers: [],
-      });
-
-      pc.createDataChannel('');
-
-      pc.createOffer().then((offer) => pc.setLocalDescription(offer));
-
-      pc.onicecandidate = (ice) => {
-        if (ice && ice.candidate && ice.candidate.candidate) {
-          const ipMatch = ipRegex.exec(ice.candidate.candidate);
-          if (ipMatch) {
-            setLocalIp(ipMatch[0]);
-            pc.onicecandidate = null; 
-          }
-        }
-      };
-    };
-
-    getLocalIP();
+    var url = location.protocol + '//' + location.host;
+    console.log(url);
   }, []);
 
     return (
