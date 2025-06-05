739fe090c3c694a2bd6611dbe6b9196edde1ee22
Lexfer <lexferramirez@gmail.com>
2025-06-05 16:46:06 -0400

    fix

--- src/app/_components/QueryResultDisplay.tsx
+++ src/app/_components/QueryResultDisplay.tsx
@@ -19,6 +19,7 @@ const QueryResultDisplay = ({
                                     justifyContent: "center",
                                     alignItems: "center",
                                     gap: "10px",
+                                    maxWidth: "300px",
                                 }}
                             >
                                 <h4 style={{ textAlign: "center", flex: 1 }}>{res.key}</h4>
