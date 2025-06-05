cae7782e22e966ca6dec6a05439968eb57017693
Lexfer <lexferramirez@gmail.com>
2025-06-05 16:39:37 -0400

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body style={{ height: "100vh" }}>
+      <body style={{ height: "100%" }}>
         {children}
       </body>
     </html>
