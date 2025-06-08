48f4b7462c9d9632a43f897c1ab21109d7e886ca
Lexfer <lexferramirez@gmail.com>
2025-06-08 08:11:46 -0400

    fix

--- src/app/camerav2/services/geminiServiceII.tsx
+++ src/app/camerav2/services/geminiServiceII.tsx
@@ -35,7 +35,7 @@ If no brand is a reasonably good match, respond with "NONE".
 
   try {
     const response = await ai.models.generateContent({
-      model: 'gemini-2.0-flash-lite',//'gemini-2.5-flash-preview-05-20',
+      model: 'gemini-2.5-flash-preview-05-20',
       contents: prompt,
     });
     
