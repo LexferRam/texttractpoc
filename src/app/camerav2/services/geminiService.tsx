b15a1ae26381379d487f72a7425740b1c10020d0
Lexfer <lexferramirez@gmail.com>
2025-06-10 12:27:20 -0400

    fix

--- src/app/camerav2/services/geminiService.tsx
+++ src/app/camerav2/services/geminiService.tsx
@@ -43,7 +43,7 @@ export const extractInfoFromImage = async (
     };
 
     // Using the specified model for multimodal capabilities
-    const model = 'gemini-2.5-flash-preview-05-20';
+    const model = "gemini-1.5-flash-latest"//'gemini-2.5-flash-preview-05-20';
 
     const response: GenerateContentResponse = await ai.models.generateContent({
       model: model,
