3e84d7eecf85292614dfec0078cd23707eead9d1
Lexfer <lexferramirez@gmail.com>
2025-06-11 15:27:48 -0400

    update models

--- src/app/camerav2/services/geminiServiceIV.tsx
+++ src/app/camerav2/services/geminiServiceIV.tsx
@@ -69,7 +69,7 @@ null
 
   try {
     const response: GenerateContentResponse = await ai.models.generateContent({
-      model: 'gemini-1.5-flash-latest',
+      model: "gemini-2.0-flash-lite",//'gemini-1.5-flash-latest',
       contents: prompt,
       config: {
         responseMimeType: "application/json",
