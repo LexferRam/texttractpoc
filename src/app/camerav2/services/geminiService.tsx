eb7892f911921b435e55f45a555d3a08aeebdce8
Lexfer <lexferramirez@gmail.com>
2025-06-14 15:54:35 -0400

    fix

--- src/app/camerav2/services/geminiService.tsx
+++ src/app/camerav2/services/geminiService.tsx
@@ -1,5 +1,7 @@
 
 import { GoogleGenAI, GenerateContentResponse, Part, Type } from "@google/genai";
+import { prompts } from "../const/prompts";
+import { title } from "process";
 
 const API_KEY = process.env.NEXT_PUBLIC_GEMINI;
 
@@ -21,7 +23,7 @@ if (API_KEY) {
 export const extractInfoFromImage = async (
   base64ImageData: string,
   mimeType: string,
-  prompt: string
+  promptType: string
 ): Promise<string | undefined> => {
   if (!ai) {
     // This error will also cover cases where API_KEY was missing or initialization failed.
@@ -31,6 +33,7 @@ export const extractInfoFromImage = async (
   }
 
   try {
+    console.log(promptType)
     const imagePart: Part = {
       inlineData: {
         mimeType: mimeType,
@@ -38,12 +41,12 @@ export const extractInfoFromImage = async (
       },
     };
 
-    const textPart: Part = {
-      text: prompt,
+    const textPart: any = {
+      text: prompts[promptType],
     };
 
     // Using the specified model for multimodal capabilities
-    const model = "gemini-2.0-flash-lite"//"gemini-1.5-flash-latest"//'gemini-2.5-flash-preview-05-20';
+    const model = 'gemini-2.5-flash-preview-05-20';//"gemini-2.0-flash-lite"//"gemini-1.5-flash-latest"//'gemini-2.5-flash-preview-05-20';
 
     const response: GenerateContentResponse = await ai.models.generateContent({
       model: model,
@@ -62,52 +65,87 @@ export const extractInfoFromImage = async (
         responseMimeType: 'application/json', // Ensure the response is in JSON format
         responseSchema: {
           type: Type.ARRAY,
-          items: {
+          items: promptType === 'CI_RIF' ? {
+            type: Type.OBJECT,
+            title: "Información extraida de la CI o el RIF",
+            description: "Información extraída de la CI o el RIF",
+            properties: {
+              numeroIdentificacion: {
+                type: Type.STRING,
+                description: "Numero de identificación, retorna string vacio si no captura valor",
+              },
+              nombre: {
+                type: Type.STRING,
+                description: "Nombres de la Cédula de identidad o RIF, retorna string vacio si no captura valor",
+              },
+              apellidos: {
+                type: Type.STRING,
+                description: "Apellidos de la Cédula de identidad o RIF, retorna string vacio si no captura valor",
+              },
+              fechaVencimiento: {
+                type: Type.STRING,
+                description: "Fecha de vencimiento de la Cédula de identidad o RIF, retorna string vacio si no captura valor",
+              },
+              documentValid: {
+                type: Type.BOOLEAN,
+                description: "Retorna true si es un documento válido (CI o RIF) y false si no lo es "
+              },
+              scannedDocument: {
+                type: Type.STRING,
+                enum: ["CI", "RIF"],
+                description: "Tipo de documento escaneado,CI: para cédula, RIF: para RIF, retorna string vacio si no captura valor ",
+              },
+              tipoIdentificacion: {
+                type: Type.STRING,
+                enum: ["V", "P", "J", "G", "E"],
+                description: "Tipo de identificación: retorna V si es Venezolano, P si es pasaporte, J si es jurídico, G si es gubernamental y E si es extranjero",
+              },
+            }
+          } : {
             type: Type.OBJECT,
             description: "Información extraída del vehículo o documento",
             title: "Vehículo o Certificado Vehicular",
             // Aquí se definen las propiedades esperadas en el JSON de respuesta
-            // Asegúrate de que coincidan con lo que esperas recibir
             properties: {
               anio: {
                 type: Type.STRING,
-                description: "Año del vehículo o documento, retorna string vacio",
+                description: "Año del vehículo o documento, retorna string vacio si no captura valor",
               },
               marca: {
                 type: Type.STRING,
-                description: "Marca del vehículo o documento, retorna string vacio",
+                description: "Marca del vehículo o documento, retorna string vacio si no captura valor",
               },
               modelo: {
                 type: Type.STRING,
-                description: "Modelo del vehículo o documento, retorna string vacio",
+                description: "Modelo del vehículo o documento, retorna string vacio si no captura valor",
               },
               placa: {
                 type: Type.STRING,
-                description: "Placa del vehículo o documento, retorna string vacio",
+                description: "Placa del vehículo o documento, retorna string vacio si no captura valor",
               },
               serialMotor: {
                 type: Type.STRING,
-                description: "Serial del motor del vehículo o documento, retorna string vacio",
+                description: "Serial del motor del vehículo o documento, retorna string vacio si no captura valor",
               },
               serialCarroceria: {
                 type: Type.STRING,
-                description: "Serial de la carrocería del vehículo o documento, retorna string vacio",
+                description: "Serial de la carrocería del vehículo o documento, retorna string vacio si no captura valor",
               },
               color: {
                 type: Type.STRING,
-                description: "Color del vehículo o documento, retorna string vacio",
+                description: "Color del vehículo o documento, retorna string vacio si no captura valor",
               },
               destinado: {
                 type: Type.STRING,
-                description: "Destinado del vehículo o documento, retorna string vacio",
+                description: "Destinado del vehículo o documento, retorna string vacio si no captura valor",
               },
               grupo: {
                 type: Type.STRING,
-                description: "Grupo del vehículo o documento, retorna string vacio",
+                description: "Grupo del vehículo o documento, retorna string vacio si no captura valor",
               },
               version: {
                 type: Type.STRING,
-                description: "Versión del vehículo o documento, retorna string vacio",
+                description: "Versión del vehículo o documento, retorna string vacio si no captura valor",
               },
             }
           },
